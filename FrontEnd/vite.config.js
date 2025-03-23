e3acc74f71fad2e80280de04b07f78dcf260080d
salimkhandev <salimt450s@gmail.com>
2025-03-23 21:18:02 +0500

    improved

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -3,14 +3,5 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+
 })
