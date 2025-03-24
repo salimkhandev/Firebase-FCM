5e1829957b3753bc96491983d1fdc806cdf71839
salimkhandev <salimt450s@gmail.com>
2025-03-24 21:57:23 +0500

    improved

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,7 +1,23 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      strategies: 'injectManifest',
+      srcDir: 'public',
+      filename: 'service-worker.js',
+      registerType: 'autoUpdate',
+      manifest: false,
+      useManifestFromFile: true,
+      injectRegister: null,
+      injectManifest: {
+        injectionPoint: undefined
+      }
+    })
+  ],
 
 })
